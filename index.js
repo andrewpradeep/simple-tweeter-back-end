4065f1f06ea24ea23ffe2b9074817968b3cc36ec
andrewpradeepj <andrewpradeep.vtpl@gmail.com>
2023-08-18 02:42:22 +0530

    feat: added websocket server

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
-const express = require('express');
-const bodyParser = require('body-parser')
+const express = require("express");
+const bodyParser = require("body-parser");
+const router = require("./routes/tasks");
+const { WebSocketServer } = require("ws");
 
 const app = express();
 
-const PORT = process.env.PORT || '3300';
+const PORT = process.env.PORT || "3300";
 
-app.use(bodyParser.json());
+const wss = new WebSocketServer({ port: 8080 });
 
-app.get('/getMyName', (req, res, next)=>{
-    const paramName = req.query.name;
-    res.status(200).json({data: paramName})
-})
+wss.on("connection", function connection(ws) {
+    ws.on("message", function message(data) {
+        console.log("received: %s", data);
+    });
 
-app.post('/getMyName', (req, res, next)=>{
-    const paramName = req.body;
-    res.status(200).json(paramName)
+    ws.send("something");
 });
 
-app.listen(PORT, function(err){
-    err ? console.log(err) : console.log('server started')
+app.use(bodyParser.json());
+app.use(router);
+
+app.get("/getMyName", (req, res, next) => {
+    const paramName = req.query.name;
+    res.status(200).json({ data: paramName });
+});
+
+app.post("/getMyName", (req, res, next) => {
+    const paramName = req.body;
+    res.status(200).json(paramName);
+});
+
+app.listen(PORT, function (err) {
+    err ? console.log(err) : console.log("server started");
 });
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,9 +1,12 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
-router.get('list', (req, res, next)=>{
-
-})
+router.get("/list", (req, res, next) => {
+    res.status(200).json([
+        { name: "File ITR", expiry: 10 },
+        { name: "Terminate EC@ instance", expiry: 2 },
+    ]);
+});
 
 module.exports = router;
